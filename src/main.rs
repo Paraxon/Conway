7b9bfcd4484a630c286c26dd80f8b47cc0ff55cf
James Metke <james_metke@hotmail.com>
2022-03-26 17:16:20 -0600

    snake case package name, 1st image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+fn main() ->std::io::Result<()> {
+    let mut file = fs::File::create("img.ppm").unwrap();
+    let width = 3;
+    let height = 2;
+    fs::write("img.ppm", "P3\n3 2\n255\n255 0 0 0 255 0 0 0 255\n255 255 0 255 255 255 0 0 0");
+    Ok(())
 }
